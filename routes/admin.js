80a43868a351e5ac60448b06876baaef9fbdd798
idmzulkarnaen <kreatifku1995@gmail.com>
2021-11-17 15:40:01 +0700

    Item Update/CRUD

--- routes/admin.js
+++ routes/admin.js
@@ -19,5 +19,6 @@ router.get('/booking', adminController.viewBooking);
 router.post('/item', uploadMultiple, adminController.addItem);
 router.get('/item/show-image/:id', adminController.showImageItem);
 router.get('/item/:id', adminController.showEditItem);
+router.put('/item/:id', uploadMultiple, adminController.editItem);
 
 module.exports = router;
