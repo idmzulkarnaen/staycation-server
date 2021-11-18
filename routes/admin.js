ae2b6f4a4a71aacad9ee02e1781d3bee5aa75c9e
idmzulkarnaen <kreatifku1995@gmail.com>
2021-11-18 12:08:57 +0700

    Activity Update/CRUD Item Detail

--- routes/admin.js
+++ routes/admin.js
@@ -29,5 +29,6 @@ router.put('/item/update/feature', uploadSingle, adminController.editFeature);
 router.delete('/item/:itemId/feature/:id', adminController.deleteFeature);
 
 router.post('/item/add/activity', uploadSingle, adminController.addActivity);
+router.put('/item/update/activity', uploadSingle, adminController.editActivity);
 
 module.exports = router;
