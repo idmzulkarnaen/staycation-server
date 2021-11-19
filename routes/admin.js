1ce089ded7c39092ceb03e91d67e98c2df1115fe
idmzulkarnaen <kreatifku1995@gmail.com>
2021-11-19 10:38:11 +0700

    Show Detail Booking

--- routes/admin.js
+++ routes/admin.js
@@ -20,7 +20,6 @@ router.put('/bank', uploadSingle, adminController.editBank);
 router.delete('/bank/:id', adminController.deleteBank);
 // endpoint item
 router.get('/item', adminController.viewItem);
-router.get('/booking', adminController.viewBooking);
 router.post('/item', uploadMultiple, adminController.addItem);
 router.get('/item/show-image/:id', adminController.showImageItem);
 router.get('/item/:id', adminController.showEditItem);
@@ -36,5 +35,8 @@ router.delete('/item/:itemId/feature/:id', adminController.deleteFeature);
 router.post('/item/add/activity', uploadSingle, adminController.addActivity);
 router.put('/item/update/activity', uploadSingle, adminController.editActivity);
 router.delete('/item/:itemId/activity/:id', adminController.deleteActivity);
+
+router.get('/booking', adminController.viewBooking);
+router.get('/booking/:id', adminController.showDetailBooking);
 
 module.exports = router;
