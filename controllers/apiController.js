dc9e153a747583f912d8fa1ae7c76b853a94a43e
idmzulkarnaen <kreatifku1995@gmail.com>
2021-11-19 15:17:02 +0700

    most picked n hero

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,6 +1,29 @@
+const Item = require('../models/Item');
+const Treasure = require('../models/Activity');
+const Treveler = require('../models/Booking');
+
 module.exports = {
     landingPage: async (req, res) => {
-        const message = 'Hello Json';
-        res.status(200).json({message})
+        try {
+            const mostPicked = await Item.find()
+                .select('_id title country city price unit imageId')
+                .limit(5)
+                .populate({ path: 'imageId', select: '_id imageUrl' })
+            
+            const treveler = await Treveler.find();
+            const treasure = await Treasure.find();
+            const city = await Item.find();
+            res.status(200).json({
+                hero: {
+                  travelers: treveler.length,
+                  treasures: treasure.length,
+                  cities: city.length
+                },
+                mostPicked,
+            })
+        } catch (error) {
+            
+        }
+        
     },
 }
