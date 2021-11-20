a51aa9d23e1ecf9de005321c58ed435205fbba29
idmzulkarnaen <kreatifku1995@gmail.com>
2021-11-20 09:37:42 +0700

    API booking selesai

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -103,7 +103,7 @@ module.exports = {
         }
     },
 
-    bookingPage: (req, res) => {
+    bookingPage: async (req, res) => {
         const {
             idItem,
             duration,
@@ -138,6 +138,55 @@ module.exports = {
             res.status(404).json({ message: "Lengkapi semua field" });
         }
 
-        res.status(201).json({ message: "Success Booking" });
+        const item = await Item.findOne({ _id: idItem });
+
+        if (!item) {
+            return res.status(404).json({ message: "Item not found" });
+        }
+
+        item.sumBooking += 1;
+
+        await item.save();
+
+        let total = item.price * duration;
+        let tax = total * 0.10;
+
+        const invoice = Math.floor(1000000 + Math.random() * 9000000);
+
+        // console.log(total);
+        // console.log(tax);
+        // console.log(invoice);
+
+        const member = await Member.create({
+            firstName,
+            lastName,
+            email,
+            phoneNumber
+        });
+
+        const newBooking = {
+            invoice,
+            bookingStartDate,
+            bookingEndDate,
+            total: total += tax,
+            itemId: {
+              _id: item.id,
+              title: item.title,
+              price: item.price,
+              duration: duration
+            },
+      
+            memberId: member.id,
+            payments: {
+              proofPayment: `images/${req.file.filename}`,
+              bankFrom: bankFrom,
+              accountHolder: accountHolder
+            }
+        }
+
+        
+        const booking = await Booking.create(newBooking);
+
+        res.status(201).json({ message: "Success Booking", booking });
     }
 }
